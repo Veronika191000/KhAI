c7364b3eeeaaca7e822ade791127468abf8487a6
vabarnikova <v.abarnikova@student.csn.khai.edu>
2021-12-12 19:32:40 +0200

    Changes for pointer branch

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Using revert command!");
+QPushButton button("Hello, World!");
 button.resize(200, 60);
 button.show( );
  
